85a523ab17c76f6e0ffcee80fe47382132f9b186
wtrawlings <rawlings.warren@gmail.com>
2020-02-23 18:58:26 -0500

    wasn't ablel to get this test working

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,17 @@
 import React from "react";
 import { render } from "@testing-library/react";
+//import { getByText } from "@testing-library/dom"
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+// test("verify text Message is in DOM", () => {
+//   //arrange by feeding the app into the
+//   const { getByText } = render(<App />);
+//   //act
+//   const checkMessage = getByText(/message/i);
+//   //assert
+//   expect(checkMessage).toBeInTheDocument();
+// });
